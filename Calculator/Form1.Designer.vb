9e5e80acb1ea38ab0ad50fae7488842c83272f2a
Lau2david <automata988@gmail.com>
2026-01-06 21:08:47 +0100

    The_Calculator

---
+++ Calculator/Form1.Designer.vb
@@ -0,0 +1,285 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Display = New TextBox()
+        Equals = New Button()
+        Button5 = New Button()
+        Button6 = New Button()
+        Button7 = New Button()
+        Button8 = New Button()
+        Button9 = New Button()
+        Button10 = New Button()
+        Button11 = New Button()
+        Button12 = New Button()
+        Button13 = New Button()
+        Button14 = New Button()
+        Button15 = New Button()
+        Button16 = New Button()
+        Button17 = New Button()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Font = New Font("Segoe UI", 11.25F)
+        Button1.Location = New Point(292, 193)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 66)
+        Button1.TabIndex = 0
+        Button1.Text = "*"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Font = New Font("Segoe UI", 11.25F)
+        Button2.Location = New Point(292, 265)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(75, 66)
+        Button2.TabIndex = 1
+        Button2.Text = "-"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Font = New Font("Segoe UI", 11.25F)
+        Button3.Location = New Point(292, 339)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(75, 103)
+        Button3.TabIndex = 2
+        Button3.Text = "+"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Font = New Font("Segoe UI", 11.25F)
+        Button4.Location = New Point(292, 121)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(75, 66)
+        Button4.TabIndex = 3
+        Button4.Text = "/"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Display
+        ' 
+        Display.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(238))
+        Display.Location = New Point(12, 12)
+        Display.Multiline = True
+        Display.Name = "Display"
+        Display.ScrollBars = ScrollBars.Horizontal
+        Display.Size = New Size(355, 87)
+        Display.TabIndex = 4
+        ' 
+        ' Equals
+        ' 
+        Equals.Font = New Font("Segoe UI", 11.25F)
+        Equals.Location = New Point(200, 383)
+        Equals.Name = "Equals"
+        Equals.Size = New Size(75, 55)
+        Equals.TabIndex = 5
+        Equals.Text = "="
+        Equals.UseVisualStyleBackColor = True
+        ' 
+        ' Button5
+        ' 
+        Button5.Font = New Font("Segoe UI", 11.25F)
+        Button5.Location = New Point(38, 312)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(75, 55)
+        Button5.TabIndex = 6
+        Button5.Text = "1"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' Button6
+        ' 
+        Button6.Font = New Font("Segoe UI", 11.25F)
+        Button6.Location = New Point(119, 312)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(75, 55)
+        Button6.TabIndex = 7
+        Button6.Text = "2"
+        Button6.UseVisualStyleBackColor = True
+        ' 
+        ' Button7
+        ' 
+        Button7.Font = New Font("Segoe UI", 11.25F)
+        Button7.Location = New Point(200, 312)
+        Button7.Name = "Button7"
+        Button7.Size = New Size(75, 55)
+        Button7.TabIndex = 8
+        Button7.Text = "3"
+        Button7.UseVisualStyleBackColor = True
+        ' 
+        ' Button8
+        ' 
+        Button8.Font = New Font("Segoe UI", 11.25F)
+        Button8.Location = New Point(38, 240)
+        Button8.Name = "Button8"
+        Button8.Size = New Size(75, 55)
+        Button8.TabIndex = 9
+        Button8.Text = "4"
+        Button8.UseVisualStyleBackColor = True
+        ' 
+        ' Button9
+        ' 
+        Button9.Font = New Font("Segoe UI", 11.25F)
+        Button9.Location = New Point(119, 240)
+        Button9.Name = "Button9"
+        Button9.Size = New Size(75, 55)
+        Button9.TabIndex = 10
+        Button9.Text = "5"
+        Button9.UseVisualStyleBackColor = True
+        ' 
+        ' Button10
+        ' 
+        Button10.Font = New Font("Segoe UI", 11.25F)
+        Button10.Location = New Point(200, 240)
+        Button10.Name = "Button10"
+        Button10.Size = New Size(75, 55)
+        Button10.TabIndex = 11
+        Button10.Text = "6"
+        Button10.UseVisualStyleBackColor = True
+        ' 
+        ' Button11
+        ' 
+        Button11.Font = New Font("Segoe UI", 11.25F)
+        Button11.Location = New Point(38, 170)
+        Button11.Name = "Button11"
+        Button11.Size = New Size(75, 55)
+        Button11.TabIndex = 12
+        Button11.Text = "7"
+        Button11.UseVisualStyleBackColor = True
+        ' 
+        ' Button12
+        ' 
+        Button12.Font = New Font("Segoe UI", 11.25F)
+        Button12.Location = New Point(119, 170)
+        Button12.Name = "Button12"
+        Button12.Size = New Size(75, 55)
+        Button12.TabIndex = 13
+        Button12.Text = "8"
+        Button12.UseVisualStyleBackColor = True
+        ' 
+        ' Button13
+        ' 
+        Button13.Font = New Font("Segoe UI", 11.25F)
+        Button13.Location = New Point(200, 170)
+        Button13.Name = "Button13"
+        Button13.Size = New Size(75, 55)
+        Button13.TabIndex = 14
+        Button13.Text = "9"
+        Button13.UseVisualStyleBackColor = True
+        ' 
+        ' Button14
+        ' 
+        Button14.Font = New Font("Segoe UI", 11.25F)
+        Button14.Location = New Point(119, 383)
+        Button14.Name = "Button14"
+        Button14.Size = New Size(75, 55)
+        Button14.TabIndex = 15
+        Button14.Text = "."
+        Button14.UseVisualStyleBackColor = True
+        ' 
+        ' Button15
+        ' 
+        Button15.Font = New Font("Segoe UI", 11.25F)
+        Button15.Location = New Point(38, 383)
+        Button15.Name = "Button15"
+        Button15.Size = New Size(75, 55)
+        Button15.TabIndex = 16
+        Button15.Text = "0"
+        Button15.UseVisualStyleBackColor = True
+        ' 
+        ' Button16
+        ' 
+        Button16.Location = New Point(38, 121)
+        Button16.Name = "Button16"
+        Button16.Size = New Size(75, 31)
+        Button16.TabIndex = 17
+        Button16.Text = "Reset"
+        Button16.UseVisualStyleBackColor = True
+        ' 
+        ' Button17
+        ' 
+        Button17.Location = New Point(200, 121)
+        Button17.Name = "Button17"
+        Button17.Size = New Size(75, 31)
+        Button17.TabIndex = 18
+        Button17.Text = "Backspace"
+        Button17.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(379, 450)
+        Controls.Add(Button17)
+        Controls.Add(Button16)
+        Controls.Add(Button15)
+        Controls.Add(Button14)
+        Controls.Add(Button13)
+        Controls.Add(Button12)
+        Controls.Add(Button11)
+        Controls.Add(Button10)
+        Controls.Add(Button9)
+        Controls.Add(Button8)
+        Controls.Add(Button7)
+        Controls.Add(Button6)
+        Controls.Add(Button5)
+        Controls.Add(Equals)
+        Controls.Add(Display)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        FormBorderStyle = FormBorderStyle.FixedToolWindow
+        Name = "Form1"
+        Text = "Calculator"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Display As TextBox
+    Friend WithEvents Equals As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents Button15 As Button
+    Friend WithEvents Button16 As Button
+    Friend WithEvents Button17 As Button
+
+End Class
---
+++ Calculator/Form1.vb
@@ -0,0 +1,84 @@
+﻿Public Class Form1
+    Private Sub Plus_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Display.Text = Display.Text & "+"
+    End Sub
+
+    Private Sub Minus_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Display.Text = Display.Text & "-"
+    End Sub
+
+    Private Sub Multiply_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Display.Text = Display.Text & "*"
+    End Sub
+
+    Private Sub Division_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Display.Text = Display.Text & "/"
+    End Sub
+
+    Private Sub Calculate_Click(sender As Object, e As EventArgs) Handles Equals.Click
+        Dim expr As String
+        expr = Display.Text
+
+        Dim result As Double
+        Try
+            result = Convert.ToDouble(New DataTable().Compute(expr, Nothing))
+            Display.Text = result.ToString()
+        Catch ex As Exception
+            MessageBox.Show("Invalid expression!")
+        End Try
+    End Sub
+
+    Private Sub one_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Display.Text = Display.Text & "1"
+    End Sub
+
+    Private Sub two_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Display.Text = Display.Text & "2"
+    End Sub
+
+    Private Sub three_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Display.Text = Display.Text & "3"
+    End Sub
+
+    Private Sub four_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Display.Text = Display.Text & "4"
+    End Sub
+
+    Private Sub five_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        Display.Text = Display.Text & "5"
+    End Sub
+
+    Private Sub six_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        Display.Text = Display.Text & "6"
+    End Sub
+
+    Private Sub seven_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        Display.Text = Display.Text & "7"
+    End Sub
+
+    Private Sub eight_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        Display.Text = Display.Text & "8"
+    End Sub
+
+    Private Sub nine_Click(sender As Object, e As EventArgs) Handles Button13.Click
+        Display.Text = Display.Text & "9"
+    End Sub
+
+    Private Sub dot_Click(sender As Object, e As EventArgs) Handles Button14.Click
+        Display.Text = Display.Text & "."
+    End Sub
+
+    Private Sub zero_Click(sender As Object, e As EventArgs) Handles Button15.Click
+        Display.Text = Display.Text & "0"
+    End Sub
+
+    Private Sub reset_Click(sender As Object, e As EventArgs) Handles Button16.Click
+        Display.Text = ""
+    End Sub
+
+    Private Sub Button17_Click(sender As Object, e As EventArgs) Handles Button17.Click
+        If Display.Text.Length > 0 Then
+            Display.Text = Display.Text.Substring(0, Display.Text.Length - 1)
+        End If
+    End Sub
+End Class
